04c2f1846c7be8b941d212dc3453a423c54fa034
Dold <dold@dt.team>
2023-10-23 12:06:38 +0300

    Додасть другий слайд та налаштування стилей

--- src/views/MainPage/MainPage.vue
+++ src/views/MainPage/MainPage.vue
@@ -1,13 +1,28 @@
 <script setup>
-import FirstSlide from "@/layouts/FirstSlide.vue";
+import FirstSlide from "@/pages/FirstSection/FirstSection.vue";
+import AboutUs from "@/pages/AboutUs/AboutUs.vue";
+import "@/styles/main.scss";
 </script>
 
 <template>
-  <v-app theme='light' class='bg-light-blue-darken-4'>
-    <v-container class="fill-height">
+  <v-app theme="light">
+    <v-container fluid fill-height>
       <v-responsive class="align-center fill-height">
         <FirstSlide />
+        <v-divider></v-divider>
+        <AboutUs />
       </v-responsive>
     </v-container>
   </v-app>
 </template>
+
+<style scoped lang="scss">
+.v-divider {
+  margin-top: 40px;
+}
+
+.v-application {
+  color: white;
+  background-color: rgb(2, 22, 48);
+}
+</style>
