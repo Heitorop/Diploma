a805f5b935a18299d0ac2a33fa0473df420417b0
Dold <dold@dt.team>
2023-10-25 18:51:02 +0300

    До футера

--- src/views/MainPage/MainPage.vue
+++ src/views/MainPage/MainPage.vue
@@ -3,9 +3,8 @@ import FirstSlide from "@/pages/FirstSection/FirstSection.vue";
 import AboutUs from "@/pages/AboutUs/AboutUs.vue";
 import "@/styles/main.scss";
 
-
 // VARIABLES
-const icons =  ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram']
+const icons = ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"];
 </script>
 
 <template>
@@ -18,31 +17,31 @@ const icons =  ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram']
       </v-responsive>
     </v-container>
     <v-footer class="d-flex flex-column">
-    <div class="bg-light-blue d-flex w-100 align-center px-4">
-      <strong>Get connected with us on social networks!</strong>
+      <div class="bg-light-blue d-flex w-100 align-center justify-center px-4">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          :icon="icon"
+          variant="plain"
+          size="small"
+        ></v-btn>
+      </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        v-for="icon in icons"
-        :key="icon"
-        class="mx-4"
-        :icon="icon"
-        variant="plain"
-        size="small"
-      ></v-btn>
-    </div>
-
-    <div class="px-4 py-2 bg-black text-center w-100">
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-    </div>
-  </v-footer>
+      <div class="px-4 py-2 bg-black text-center w-100">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
 <style scoped lang="scss">
 .v-divider {
   margin-top: 40px;
+}
+.v-footer {
+  padding: 0;
+  justify-content: center;
 }
 
 .v-application {
